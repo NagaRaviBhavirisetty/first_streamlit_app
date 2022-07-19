e58cf741ffa31a8d3393a5f3fd9c8fde2056f466
NagaRaviBhavirisetty <109619115+NagaRaviBhavirisetty@users.noreply.github.com>
2022-07-19 23:04:56 +0530

    Update streamlit_app.py
    made to smallercase

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,9 +2,7 @@
 import streamlit
 streamlit.title('My parents New Healthy Diner')
 
-Streamlit.header('Breakfast Menu')
-Streamlit.text('Omega 3 & Blueberry Oatmeal')
-Streamlit.text('Kale,Spinach & Rocket Smoothie')
-Streamlit.text('Hard-Boiled Free-Range Egga')
-
-
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale,Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egga')
